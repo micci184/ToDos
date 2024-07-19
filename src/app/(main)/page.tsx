66b652445b8435caa44290412c1f0171f7264217
micci184 <micci184@gmail.com>
2024-07-19 11:00:12 +0900

    add useState

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,26 +1,33 @@
+"use client";
+
+import React, { useState } from "react";
 import TodoForm from "@/components/Todo/TodoForm";
 import TodoItemList from "@/components/Todo/TodoItemList";
 import { TodoItemProps } from "@/components/Todo/TodoItem";
 
-const todoItems: TodoItemProps[] = [
-  {
-    title: "Sample 1",
-    content: "Your account was registered!",
-    status: "Done",
-  },
-  {
-    title: "Sample 2",
-    content: "Your account was verified!",
-    status: "Progress",
-  },
-  {
-    title: "Sample 3",
-    content: "Your account was updated!",
-    status: "Incomplete",
-  },
-];
-
 export default function Home() {
+  const [todoItems, setTodoItems] = useState<TodoItemProps[]>([
+    {
+      title: "Sample 1",
+      content: "Your account was registered!",
+      status: "Done",
+    },
+    {
+      title: "Sample 2",
+      content: "Your account was verified!",
+      status: "Progress",
+    },
+    {
+      title: "Sample 3",
+      content: "Your account was updated!",
+      status: "Incomplete",
+    },
+  ]);
+
+  const addTodo = (title: string, content: string) => {
+    setTodoItems([...todoItems, { title, content, status: "Incomplete" }]);
+  };
+
   return (
     <div>
       <h1 className="text-center text-2xl text-slate-600 font-bold mb-4 pt-4">
@@ -28,7 +35,7 @@ export default function Home() {
       </h1>
       <TodoItemList data={todoItems} />
       <div className="mt-2">
-        <TodoForm />
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
--- src/components/Todo/TodoForm.tsx
+++ src/components/Todo/TodoForm.tsx
@@ -1,20 +1,47 @@
-const TodoForm = () => {
-	return (
-    <div className="max-w-sm mx-auto">
+import React, { useState } from "react";
+
+type TodoFormProps = {
+  addTodo: (title: string, content: string) => void;
+};
+
+const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    addTodo(title, content);
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-sm mx-auto">
       <label className="block text-sm text-gray-500 dark:text-gray-300">
         Input task name
       </label>
       <input
         type="text"
-        placeholder="sample"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         className="block w-full mt-2 placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
       />
-
+      <label className="block text-sm text-gray-500 dark:text-gray-300 mt-4">
+        Input task content
+      </label>
+      <input
+        type="text"
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="block w-full mt-2 placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
+      />
       <button className="w-full mt-2 px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-        Primary
+        Add
       </button>
-    </div>
+    </form>
   );
-}
+};
 
-export default TodoForm
+export default TodoForm;
